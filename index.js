eb9548a63870f8ca27e61de98df17c90053d22ec
Payalaher <162550484+Payalaher@users.noreply.github.com>
2024-06-15 16:15:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    freeMode: true,
+    loop:true,
+    autoplay : {
+      delay:1500
+    },
+    breakpoint:{
+      375:{
+        slidesPerView:6,
+        spaceBetween:20,
+
+      }
+  
+    },
+  
+  });
+
+  var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    freeMode: true,
+    loop:true,
+    autoplay : {
+      delay:1500
+    }
+    
+  });
+
+
+
+  var swiper = new Swiper(".mySwiper4", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    loop:true,
+    autoplay : {
+      delay:1500
+    }
+    
+  });
+
+
+  var swiper = new Swiper(".mySwiper5", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    loop:true,
+    autoplay : {
+      delay:1500
+    }
+    
+  });
+
+  var swiper = new Swiper(".mySwiper6", {
+    slidesPerView: 3.5,
+    spaceBetween: 30,
+    freeMode: true,
+    loop:true,
+    autoplay : {
+      delay:1500
+    }
+    
+  });
+
